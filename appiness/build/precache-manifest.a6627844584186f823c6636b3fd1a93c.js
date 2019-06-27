d9a9ab489c1d633d9336d07a28bfed4bee7672aa
Karthik Padav <karthikpadavr@gmail.com>
2019-06-27 13:27:32 +0530

    init

---
+++ appiness/build/precache-manifest.a6627844584186f823c6636b3fd1a93c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cc0f331a1c545bb21af8caab7d73c8",
+    "url": "/karthik-padav/Appiness/tree/master/appiness/build/index.html"
+  },
+  {
+    "revision": "2dafc65a57b650d16d72",
+    "url": "/karthik-padav/Appiness/tree/master/appiness/build/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "05e2e7d75452d7533368",
+    "url": "/karthik-padav/Appiness/tree/master/appiness/build/static/css/main.70f37639.chunk.css"
+  },
+  {
+    "revision": "2dafc65a57b650d16d72",
+    "url": "/karthik-padav/Appiness/tree/master/appiness/build/static/js/2.f43aace7.chunk.js"
+  },
+  {
+    "revision": "05e2e7d75452d7533368",
+    "url": "/karthik-padav/Appiness/tree/master/appiness/build/static/js/main.80efe828.chunk.js"
+  },
+  {
+    "revision": "06bbddf188e2d1e681e2",
+    "url": "/karthik-padav/Appiness/tree/master/appiness/build/static/js/runtime~main.3e21b4a6.js"
+  }
+]);
